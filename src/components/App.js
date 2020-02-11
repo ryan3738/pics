86cc21f6248f002ef4b4dbd85923a79aaac5a37a
ryan3738 <ryan3738@gmail.com>
2020-02-10 22:23:18 -0800

    added promise for api data.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID 3af32b83b91c57ba297082acac5f7a36d68d3a373fc0a6ebf398810951a50318"
+        }
+      })
+      .then(response => {
+        console.log(response.data.results);
+      });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
