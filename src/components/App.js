f74626da8d948d957f26d5d3558336c471b714e5
ryan3738 <ryan3738@gmail.com>
2020-02-11 19:05:49 -0800

    Turned onSearchSubmit into arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -11,14 +13,14 @@ class App extends React.Component {
           "Client-ID 3af32b83b91c57ba297082acac5f7a36d68d3a373fc0a6ebf398810951a50318"
       }
     });
-
-    console.log(response.data.results);
-  }
+    this.setState({ images: response.data.resutls });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
