ae6ab40153e553ca3c490c216825647cbd0b92d5
ryan3738 <ryan3738@gmail.com>
2020-04-06 11:43:31 -0700

    feat: callbacks on image load
    feat: dynamic spans

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,19 +4,30 @@ class ImageCard extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = { spans: 0 };
+
     //How to create a reference, "React.createRef() assigned to a variable."
     this.imageRef = React.createRef();
   }
 
   componentDidMount() {
-    console.log(this.imageRef);
-    console.log(this.imageRef.current.clientHeight);
+    //set callback as an argument
+    this.imageRef.current.addEventListener("load", this.setSpans);
   }
+
+  //Set as an arrow function to bind the callback
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 1 + 10);
+
+    this.setState({ spans });
+  };
 
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         {/* Here is the image reference to this.imageRef */}
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
