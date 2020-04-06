ef6e04220470717a3a8ee5028b386b16de5988a1
ryan3738 <ryan3738@gmail.com>
2020-04-06 11:23:58 -0700

    feat: Accessing image height using React.createRef

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,13 +4,20 @@ class ImageCard extends React.Component {
   constructor(props) {
     super(props);
 
+    //How to create a reference, "React.createRef() assigned to a variable."
     this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log(this.imageRef);
+    console.log(this.imageRef.current.clientHeight);
   }
 
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
+        {/* Here is the image reference to this.imageRef */}
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
